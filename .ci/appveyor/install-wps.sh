b197f78a380cdb47d5881f387a8c09be660aacf5
Maik Riechert <maik.riechert@arcor.de>
2019-02-13 21:54:18 +0000

    cleanup

--- .ci/appveyor/install-wps.sh
+++ .ci/appveyor/install-wps.sh
@@ -11,7 +11,7 @@ mkdir build && cd build
 
 # TODO remove -DMPI_* variables once these get auto-detected
 # Note that -DMODE alone decides whether MPI is used or not.
-CC=gcc FC=gfortran cmake -G "MSYS Makefiles" \
+cmake -G "MSYS Makefiles" \
     -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=install -DWRF_DIR=../../WRF/build \
     -DENABLE_GRIB1=${GRIB1} -DENABLE_GRIB2_PNG=${GRIB2} -DENABLE_GRIB2_JPEG2000=${GRIB2} \
     -DMPI_INCLUDE_PATH=$MINGW_PREFIX/include -DMPI_C_LIBRARY="$MSMPI_LIB64/msmpi.lib" \
