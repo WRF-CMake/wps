abbade23feed9266f149977b3854badc30109bd9
Maik Riechert <maik.riechert@arcor.de>
2019-07-05 19:23:52 +0100

    align with wrf-cmake

--- .ci/unix/delocate.sh
+++ .ci/unix/delocate.sh
@@ -10,7 +10,9 @@ cd $SCRIPTDIR/../..
 if [ "$(uname)" == "Darwin" ]; then
 
     pip3 install delocate
+    delocate-listdeps --all --depending build/install
     delocate-path build/install
+    delocate-listdeps --all --depending build/install
 
 else
     echo "Unknown OS: $(uname)"
